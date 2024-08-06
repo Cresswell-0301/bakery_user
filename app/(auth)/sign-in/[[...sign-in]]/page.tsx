2a780ff7fc4d63dd10258142b0d9b988119f2822
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2024-08-06 20:12:15 +0800

    add logo in sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center">
+      <img src="/logo.png" alt="Logo" className="" />
       <SignIn />
     </div>
   );

e27acf41c546a31946fdc8fafbd51ad0b687a486
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2024-08-06 20:27:16 +0800

    update login required to no required

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+  // Must Login first ( Local )
+  // publicRoutes: ["/api/:path*"],
+
+  // No Required Login ( Deploy )
+  publicRoutes: ["/"],
 });
 
 export const config = {
